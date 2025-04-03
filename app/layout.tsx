136adf14655eafbb6ee0b141b6c659f804cf3fa6
fjaskotka <fabian_jaskotka@apple.com>
2025-04-02 23:00:37 -0700

    adding manifest and logos to public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,12 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Fabian Jaskotka',
   description: 'Personal website of Fabian Jaskotka',
+  manifest: '/manifest.json',
   icons: {
     icon: '/favicon.ico', // This references the favicon.ico in the app directory
+    apple: '/apple-icon.png',  // Add this if you want iOS support
   },
+  themeColor: '#869F5C',  // Should match your manifest theme_color
 }
 
 export default function RootLayout({
